543e4633e2addf07f17293b90bfe133b58f92c4a
inexxt <jac.karwowski@gmail.com>
2015-08-11 04:49:36 +0100

    changes to run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,8 @@ chmod a+x mongod-start
 
 ./mongod-start
 
-mongo rtr
+mongo rtr&
+echo "server is running on localhost:3000"
+echo "if you want to register a new user, uncomment the 'register' div in the ./views/index.hbs file"
+ 
+./bin/www
